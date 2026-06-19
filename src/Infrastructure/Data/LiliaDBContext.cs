9ebb7e1cb46c8ef81872347df78c80e41a9e379a
LXD-002-GNESREK <LXD-002-GNESREK@fedora.local>
2026-06-19 17:05:35 +0200

    Feature: Configuration of Relational Mapping. Resolves #13

--- src/Infrastructure/Data/LiliaDBContext.cs
+++ src/Infrastructure/Data/LiliaDBContext.cs
@@ -2,12 +2,15 @@
 
 using Microsoft.EntityFrameworkCore;
 using Lilia.Domain.Entities;
+using Lilia.Domain.Entities.Units;
 
 namespace Lilia.Infrastructure.Data
 {
     public class LiliaDBContext : DbContext
     {
         public DbSet<Player> Players { get; set; }
+        public DbSet<Organisation> Organisations { get; set; }
+        public DbSet<BaseUnit> Units { get; set; }
 
         public LiliaDBContext(DbContextOptions<LiliaDBContext> options) : base(options) {}
 
@@ -25,7 +28,34 @@ namespace Lilia.Infrastructure.Data
                     uc.ToJson();
                 });
 
-                entity.Ignore(p => p.Organisations);
+                entity.HasMany(p => p.Organisations)
+                      .WithOne(o => o.Player)
+                      .HasForeignKey(o => o.PlayerDiscordID)
+                      .OnDelete(DeleteBehavior.SetNull);
+            });
+
+            modelBuilder.Entity<Organisation>(entity =>
+            {
+                entity.HasKey(o => o.InternalID);
+                entity.HasIndex(o => new { o.PlayerDiscordID, o.ShortCode }).IsUnique();
+
+                entity.HasMany(o => o.SubOrganisations)
+                      .WithOne(o => o.ParentOrganisation)
+                      .HasForeignKey(o => o.ParentOrganisationID)
+                      .OnDelete(DeleteBehavior.Cascade);
+                
+                entity.HasMany(o => o.AssignedUnits)
+                      .WithOne(u => u.Organisation)
+                      .HasForeignKey(u => u.OrganisationID)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<BaseUnit>(entity =>
+            {
+                entity.HasKey(u => u.InternalID);
+
+                entity.HasDiscriminator<string>("UnitType")
+                      .HasValue<Starfighter>("Starfighter");
             });
         }
     }
